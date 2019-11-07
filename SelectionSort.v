fcb81816d5b9783aaa3da58e17decc88d21ccfe5
José Oliveira <josecarlosdeoliveirajr@gmail.com>
2019-11-07 13:54:22 -0300

    Selection Sort.

--- SelectionSort.v
+++ SelectionSort.v
@@ -124,3 +124,158 @@ Qed.
 
 
 (** * [select] selects the smallest element of a list *)
+
+Lemma select_smallest_aux:
+  forall x al y bl,
+    Forall (fun z => y <= z) bl ->
+    select x al = (y,bl) ->
+    y <= x.
+Proof.
+  intros.
+  pose (select_perm x al).
+  rewrite H0 in y0.
+  apply (Permutation_in x) in y0. {
+    destruct y0. {
+      omega.
+    } {
+      rewrite Forall_forall in H.
+      apply H.
+      apply H1.
+    }
+  } {
+    apply in_eq.
+  }
+Qed.
+
+Theorem select_smallest:
+  forall x al y bl, select x al = (y,bl) ->
+     Forall (fun z => y <= z) bl.
+Proof.
+  intros x al.
+  revert x.
+  induction al; intros; simpl in *. {
+    inv H.
+    easy.
+  } {
+    bdestruct (x <=? a). {
+      destruct select eqn:Seq.
+      inv H.
+      apply Forall_cons. {
+        apply (le_trans y x a). {
+          apply (select_smallest_aux x al y l). {
+            apply (IHal x y l).
+            easy.
+          } {
+            easy.
+          }
+        } {
+          easy.
+        }
+      } {
+        apply (IHal x y l).
+        easy.
+      }
+    } {
+      destruct (select a al) eqn:?H.
+      inv H.
+      apply Forall_cons. {
+        assert (y <= a). {
+          apply (select_smallest_aux a al y l). {
+            apply (IHal a y l).
+            apply H1.
+          } {
+            apply H1.
+          }
+        }
+        omega.
+      } {
+        apply (IHal a y l).
+        easy.
+      }
+    }
+  }
+Qed.
+
+
+(** * A list applied with selection sort is sorted **)
+
+Lemma selection_sort_sorted_aux:
+  forall  y bl,
+   sorted (selsort bl (length bl)) ->
+   Forall (fun z : nat => y <= z) bl ->
+   sorted (y :: selsort bl (length bl)).
+Proof.
+  induction bl. {
+    simpl.
+    intros.
+    apply sorted_1.
+  } {
+    intros.
+    simpl in *.
+    destruct select eqn:Seq.
+    apply sorted_cons. {
+      rewrite Forall_forall in H0.
+      apply H0.
+      apply Permutation_in with (n :: l). {
+        pose (select_perm a bl).
+        rewrite Seq in y0.
+        apply Permutation_sym.
+        apply y0.
+      } {
+        apply in_eq.
+      }
+    } {
+      apply H.
+    }
+  }
+Qed.
+
+Theorem selection_sort_sorted:
+  forall al, sorted (selection_sort al).
+Proof.
+  intros.
+  unfold selection_sort.
+  remember (length al) as n.
+  generalize dependent al.
+  induction n. {
+    intros.
+    simpl.
+    destruct al. {
+      apply sorted_nil.
+    } {
+      apply sorted_nil.
+    }
+  } {
+    intros.
+    destruct al. {
+      apply sorted_nil.
+    } {
+      unfold selsort.
+      fold selsort.
+      destruct (select n0 al) eqn:Seq.
+      pose (select_perm n0 al).
+      rewrite Seq in y.
+      apply Permutation_length in y.
+      rewrite <- Heqn in y.
+      inversion y.
+      apply (selection_sort_sorted_aux n1 l). {
+        rewrite <- H0.
+        apply IHn.
+        apply H0.
+      } {
+        apply (select_smallest n0 al n1 l).
+        apply Seq.
+      }
+    }
+  }
+Qed.
+
+
+(** * Wrapping up **)
+
+Theorem selection_sort_is_correct: selection_sort_correct.
+Proof.
+  split.
+  apply selection_sort_perm.
+  apply selection_sort_sorted.
+Qed.
